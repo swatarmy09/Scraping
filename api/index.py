7673dc96f36e443ce2354542e1ef6211c7b3e9a7
Mehul Patel <swatmehul67@gmail.com>
2025-09-01 20:14:53 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,14 @@
+import os
+import json
+
 def handler(request):
-    try:
-        # Debug environment
-        debug = {
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({
             "has_private_key": bool(os.environ.get("FIREBASE_PRIVATE_KEY")),
             "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-            "client_id": os.environ.get("FIREBASE_CLIENT_ID")
-        }
-        print("DEBUG ENV:", debug)
+            "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+            "project_id": os.environ.get("FIREBASE_PROJECT_ID", "not set")
+        })
+    }
